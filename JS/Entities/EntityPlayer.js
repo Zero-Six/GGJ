e2488189a01976560254e94ef2390882117a3e70
Pyrofoux <31957620+Pyrofoux@users.noreply.github.com>
2018-01-27 12:12:01 +0100

    Focntions de base joueur

--- JS/Entities/EntityPlayer.js
+++ JS/Entities/EntityPlayer.js
@@ -6,7 +6,9 @@ class EntityPlayer extends EntityWalking {
 		this.battery = 3;
 		this.nextAction = null;
 		this.canMove = true;
+		this.canCombo = true;
 		this.sprite = PIXI.Texture.fromFrame("j1_1.png");
+		this.combo = [];
 		
 		Program.App().stage.addChild(this.sprite);
 		
@@ -24,6 +26,68 @@ class EntityPlayer extends EntityWalking {
 	destroy()
 	{
 		Program.App().stage.removeChild(this.sprite);
+	}
+	
+	up()
+	{
+		if(this.canWalk)
+		{
+			this.vy = -Config.PlayerSpeed;
+		}
+	}
+	
+	down()
+	{
+		if(this.canWalk)
+		{
+			this.vy = +Config.PlayerSpeed;
+		}
+	}
+	
+	left()
+	{
+		if(this.canWalk)
+		{
+			this.vx = -Config.PlayerSpeed;
+		}
+	}
+	
+	left()
+	{
+		if(this.canWalk)
+		{
+			this.vy = +Config.PlayerSpeed;
+		}
+	}
+	
+	action1()
+	{
+		if(this.canCombo)
+		{
+			this.addCombo(1);
+		}
+	}
+	
+	action2()
+	{
+		this.addCombo(2);
+	}
+	
+	addCombo(c)
+	{
+		if(this.combo.length <3) // <3
+		{
+			this.combo.push(c);
+			
+			if(combo.length == 3)//combo atteint
+			{
+				
+			}
+		}
+		else
+		{
+			
+		}
 	}
 	
 	
