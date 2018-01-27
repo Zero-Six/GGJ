dd396fc8c9bfe3f0cb1193d86229399282f60b24
Pyrofoux <31957620+Pyrofoux@users.noreply.github.com>
2018-01-27 17:07:11 +0100

    push nul à chier entity

--- JS/Entities/Entity.js
+++ JS/Entities/Entity.js
@@ -79,5 +79,30 @@ class Entity
 		{
 			
 		}
+		
+		
+		getArea(r)
+		{
+			var cx = Math.floor(this.sprite1.x/Config.TileSize);
+			var cy = Math.floor(this.sprite1.y/Config.TileSize);
+			var list = [[0,0]];
+			for(var x = 1; x <= r;x++)
+			{
+				for(var y = 1; y <= x;y++)
+				{
+					list.push([cx+x,cy+y]);
+					list.push([cx-x,cy+y]);
+					list.push([cx-x,cy-y]);
+					list.push([cx+x,cy-y]);
+				}
+			}
+			
+			return list
+			
+			
+		}
+		
+		
+		
 
 }
