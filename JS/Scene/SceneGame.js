a4110d1643f9c13bf4aa0cbde625e41e524e5629
Clovis Portron <chaipokoi@gmail.com>
2018-01-27 11:09:54 +0100

    Ajout placeholder génération, Ajout image démo

--- JS/Scene/SceneGame.js
+++ JS/Scene/SceneGame.js
@@ -23,7 +23,7 @@ class SceneGame extends Scene
         this.controllers.push(new KeyboardController(null, 90, 83, 81, 68, 65, 69));
         this.controllers.push(new KeyboardController(null, 38, 40, 37, 39, 96, 110));
 
-        let grid = null; //TODO: générer la grid
+        let grid = lavender_gen(LAVENDER_ALGORITHM_BACKTRACKING, DateTime.now(), Config.MapWith, Config.MapHeight);
         this.map1 = new GameMap(Config.MapWith, Config.MapHeight, grid);
         this.map2 = new GameMap(Config.MapWith, Config.MapHeight, grid);
 
